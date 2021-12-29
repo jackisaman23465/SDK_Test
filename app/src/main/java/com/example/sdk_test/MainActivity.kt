067f393c41e6e798dc093594d048f6525af24e32
jackisaman23465 <72491009+jackisaman23465@users.noreply.github.com>
2021-12-29 14:10:34 +0800

    5

--- app/src/main/java/com/example/sdk_test/MainActivity.kt
+++ app/src/main/java/com/example/sdk_test/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.sdk_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testtoast.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast().ToastString(this)
     }
 }
